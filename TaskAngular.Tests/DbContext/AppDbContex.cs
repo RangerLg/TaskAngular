699da275bf7b00faa626791745dbe7758903ac1e
Nikita Manko <n.manko@itransition.com>
2021-10-18 15:17:47 +0300

    Selenium

---
+++ TaskAngular.Tests/DbContext/AppDbContex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskAngular.Resource.api.Models;
+using Account = TaskAngular.Auth.api.Models.Account;
+
+namespace TaskAngular.Tests.DbContext
+{
+    public class AppDbContex:Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AngularAccounts;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+    }
+}
